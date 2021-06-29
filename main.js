e78f6d8ed54ee83fb12880e9ce5cc4683165b01b
Lakshantha Dissanayake <lakshanthad@yahoo.com>
2021-06-29 22:39:44 +0530

    Update: Add comments to codes

--- main.js
+++ main.js
@@ -1,31 +1,34 @@
-var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-var LED = new Gpio(24, 'out'); //use GPIO pin 4, and specify that it is output
+var Gpio = require('onoff').Gpio; //include onoff module to interact with the GPIO
+var LED = new Gpio(24, 'out'); //initialize GPIO 24 as an output
 
+// Include app, BrowserWindow and ipcMain module
 const { app, BrowserWindow, ipcMain } = require('electron')
-ipcMain.on("msg",(event,data)=>{
-  LED.writeSync(data);
-})
-
+/* Use ipcMain module to receive the messages from the ipcRenderer module and turn ON the GPIO*/
 ipcMain.on("msg1",(event,data)=>{
   LED.writeSync(data);
 })
 
+// Use ipcMain module to receive the messages from the ipcRenderer module and turn OFF the GPIO
+ipcMain.on("msg2",(event,data)=>{
+  LED.writeSync(data);
+})
+
+// Use ipcMain module to receive the messages from the ipcRenderer module and close the app
 ipcMain.on("close-me", (event, arg) => {
     app.quit()
 })
 
+// Create application window
 app.on('ready', function() {
     var mainWindow = new BrowserWindow({
+        // Make the app fullscreen
         "fullscreen": true,
         webPreferences: {
+            // enable the communication between the main and rendered process
             nodeIntegration: true,
             contextIsolation: false
           }
     });
-    mainWindow.maximize();
+    // Load the HTML page with CSS styling
     mainWindow.loadFile('index.html');
 });
-
-try {
-    require('electron-reloader')(module)
-  } catch (_) {}
